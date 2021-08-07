bea70296000b5988ad87da24e27cf30f069ba8ec
Ashley Khoo <ashleykhoo2@gmail.com>
2021-08-08 01:19:49 +0800

    finished writing heuristic 1

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -49,18 +49,52 @@ int group[3700000];
 
 bool vis[1100000];
 
+const int FWD=3;
+const int FWD_SZ=pow(6,FWD)+0.5;
+
+const int NUM_ROUNDS=100;
+const int SHOTS_TIME=10; //in seconds
+
 void dfs(int i,vector<int> &ans){
+	ans.push_back(i);
 	vis[group[i]]=true;
 	
-	vector<int> v={0,1,2,3,4,5};
+	vector<int> v(FWD_SZ);
+	iota(v.begin(),v.end(),0);
 	shuffle(v.begin(),v.end(),rng);
 	
+	int best=0;
+	int nxt=-1;
+	
 	for (int x:v){
-		if (!vis[group[al[i][x]]]){
-			ans.push_back(x);
-			dfs(al[i][x],ans);
-			ans.push_back(x^1);
+		int temp=x%6;
+		
+		int num=0;
+		int curr=i;
+		rep(y,0,FWD){
+			curr=al[curr][x%6]; x/=6;
+			if (vis[group[curr]]) break;
+			num++;
 		}
+		
+		if (num>best){
+			best=num;
+			nxt=temp;
+			
+			if (best==FWD) break;
+		}
+	}
+	
+	if (nxt!=-1) dfs(al[i][nxt],ans);
+}
+
+void dfs2(int i,vector<int> &ans){
+	vis[group[i]]=true;
+	
+	rep(x,0,6) if (!vis[group[al[i][x]]]){
+		ans.push_back(x);
+		dfs2(al[i][x],ans);
+		ans.push_back(x^1);
 	}
 }
 
@@ -82,10 +116,8 @@ int main(){
 		IDX++;
 	}
 	
-	int best=1e9;
-	int element[24]; //shortest with order 36
+	vector<int> meo;
 	
-	int IDX2=0;
 	rep(x,0,IDX){
 		int curr[24];
 		iota(curr,curr+24,0);
@@ -101,70 +133,122 @@ int main(){
 			if (identity) break;
 		}
 		
-		if (ord==36 && moves[x].size()<best){
-			best=moves[x].size();
-			rep(y,0,24) element[y]=states[x][y];
+		if (ord==36){
+			meo.push_back(x);
 		}
 	}
 	
-	cout<<best<<endl;
-	
-	int GIDX=0;
-	memset(group,-1,sizeof(group));
-	
-	rep(x,0,IDX) if (group[x]==-1){
-		int curr[24];
-		rep(y,0,24) curr[y]=states[x][y];
+	for (int trials=0;trials<1000;trials++){
+		int element[24]; //element of meo
+		int meo_idx=rng()%meo.size();
 		
-		while (true){
-			ll temp=get_id(curr);
-			group[id[temp]]=GIDX;
+		rep(x,0,24) element[x]=states[meo[meo_idx]][x];
+		
+		int GIDX=0;
+		memset(group,-1,sizeof(group));
+		
+		rep(x,0,IDX) if (group[x]==-1){
+			int curr[24];
+			rep(y,0,24) curr[y]=states[x][y];
 			
-			conv(element,curr,curr); //order matters here!
+			while (true){
+				ll temp=get_id(curr);
+				group[id[temp]]=GIDX;
+				
+				conv(element,curr,curr); //order matters here!
+				
+				bool identity=true;
+				rep(y,0,24) if (curr[y]!=states[x][y]) identity=false;
+				
+				if (identity) break;
+			}
 			
-			bool identity=true;
-			rep(y,0,24) if (curr[y]!=states[x][y]) identity=false;
-			
-			if (identity) break;
+			GIDX++;
 		}
 		
-		GIDX++;
+		cout<<GIDX<<" "<<GIDX*36<<endl;
+		
+		
+		//precompute adjacentcy list to make runtime faster
+		rep(x,0,IDX){
+			cube c=cube();
+			
+			for (auto ch:moves[x]){
+				c.move(ch-'0');
+			}
+			
+			rep(y,0,6){
+				c.move(y);
+				al[x][y]=id[c.get_id()];
+				c.move(y^1);
+			}
+		}
+		
+		memset(vis,false,sizeof(vis));
+		
+		vis[IDX-1]=1;
+		
+		vector<int> unused(IDX-1);
+		iota(unused.begin(),unused.end(),0);
+		
+		vector<vector<int> > chain;
+		for (int rounds=0;rounds<NUM_ROUNDS;rounds++){
+			vector<int> best;
+			
+			auto start = std::chrono::system_clock::now();
+			while (((std::chrono::duration<double>)(std::chrono::system_clock::now()-start)).count()<SHOTS_TIME){
+				int source=unused[rng()%unused.size()];
+				vector<int> temp;
+				dfs(source,temp);
+				reverse(temp.begin(),temp.end());
+				temp.pop_back();
+				dfs(source,temp);
+				
+				for (auto it:temp) vis[group[it]]=false;
+				
+				if (temp.size()>best.size()) best=temp;
+			}
+			
+			cout<<best.size()<<endl;
+			for (auto it:best){
+				vis[group[it]]=true;
+			}
+			
+			vector<int> temp;
+			for (auto it:unused) if (!vis[group[it]]) temp.push_back(it);
+			
+			swap(unused,temp);
+			
+			chain.push_back(best);
+		}
+		
+		rep(x,0,chain.size()) swap(chain[x],chain[rng()%(x+1)]);
+		
+		vector<int> ans;
+		
+		dfs2(IDX-1,ans);
+		shortest_moves(states[IDX-1],states[chain[0].front()],ans);
+		rep(x,0,NUM_ROUNDS){
+			rep(y,0,chain[x].size()-1){
+				dfs2(chain[x][y],ans);
+				
+				int nxt=-1;
+				rep(z,0,6) if (al[chain[x][y]][z]==chain[x][y+1]){
+					nxt=z;
+				}
+				ans.push_back(nxt);
+			}
+			
+			dfs2(chain[x].back(),ans);
+			if (x!=NUM_ROUNDS-1) shortest_moves(states[chain[x].back()],states[chain[x+1].front()],ans);
+			else shortest_moves(states[chain[x].back()],element,ans);
+		}
+		
+		cout<<"size of devils algorithm: "<<ans.size()<<endl;
+		
+		//dump moves to a textfile
+		ofstream out("../devil/"+to_string(ans.size())+".txt");
+		for (auto it:ans) out<<it;
+		out.close();
 	}
-	
-	cout<<GIDX<<" "<<GIDX*36<<endl;
-	
-	
-	//precompute adjacentcy list to make runtime faster
-	rep(x,0,IDX){
-		cube c=cube();
-		
-		for (auto ch:moves[x]){
-			c.move(ch-'0');
-		}
-		
-		rep(y,0,6){
-			c.move(y);
-			al[x][y]=id[c.get_id()];
-			c.move(y^1);
-		}
-	}
-	cout<<"precomputation done"<<endl;
-	
-	
-	memset(vis,false,sizeof(vis));
-		
-	vector<int> ans;
-	dfs(IDX-1,ans);
-	
-	int arr[24];
-	rep(x,0,24) arr[x]=x;
-	shortest_moves(arr,element,ans);
-	
-	cout<<ans.size()<<endl;
-	
-	//dump moves to a textfile
-	ofstream out("devil.txt");
-	for (auto it:ans) out<<it;
-	out.close();
-	
 }
